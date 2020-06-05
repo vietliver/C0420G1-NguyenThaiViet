ef5b1a4a38a70617486d2b71c7a1415af16368d4
JOY <joy@JOYs-iMac-Pro.local>
2020-06-05 21:01:34 +0700

    add code

--- Module2_Java/ClassAndObjectInJava/src/BaiTap/XayDungStopWatch.java
+++ Module2_Java/ClassAndObjectInJava/src/BaiTap/XayDungStopWatch.java
@@ -3,24 +3,35 @@ import java.util.Date;
 
 public class XayDungStopWatch {
     public static void main(String[] args) {
-
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
     }
 }
 
 class StopWatch {
-    private int startTime;
-    private int endTime;
-    public void getStartTime() {
+    private long startTime;
+    private long endTime;
+    public StopWatch() {
+        this.startTime = new Date().getTime();
+    }
+
+    public long getStartTime() {
         return this.startTime;
     }
-    public void getEndTime() {
+    public long getEndTime() {
         return this.endTime;
     }
 
-
     public void start() {
-        this.startTime = new Date();
+        this.startTime = new Date().getTime();
     }
-
+    public void stop() {
+        this.endTime = new Date().getTime();
+    }
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
 
 }
