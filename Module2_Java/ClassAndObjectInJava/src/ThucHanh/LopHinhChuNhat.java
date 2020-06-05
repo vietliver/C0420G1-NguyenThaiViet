0195f730a69c534ecee667346dab43237d624a19
JOY <joy@JOYs-iMac-Pro.local>
2020-06-05 15:01:09 +0700

    add code

---
+++ Module2_Java/ClassAndObjectInJava/src/ThucHanh/LopHinhChuNhat.java
@@ -0,0 +1,37 @@
+package ThucHanh;
+
+import java.util.Scanner;
+
+public class LopHinhChuNhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
+
+class Rectangle {
+    double width, height;
+    public Rectangle () {
+    }
+    public Rectangle (double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea() {
+        return this.width * this.height;
+    }
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
+
