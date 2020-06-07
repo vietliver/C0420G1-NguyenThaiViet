a03c8190fed8aee06cfa45797340361bdcda8b08
JOY <joy@JOYs-iMac-Pro.local>
2020-06-07 14:30:34 +0700

    add code

---
+++ Module2_Java/AccessModifierAndStatic/src/ThucHanh/AccessModifier.java
@@ -0,0 +1,60 @@
+package ThucHanh;
+
+public class AccessModifier {
+}
+
+/*class A{
+    private int data=40;
+    private void msg(){System.out.println("Hello java");}
+}
+public class Simple{
+    public static void main(String args[]){
+        A obj=new A();
+        System.out.println(obj.data);//Compile Time Error
+        obj.msg();//Compile Time Error
+    }
+}*/
+
+/*
+package pack;
+class A{
+    void msg(){System.out.println("Hello");}
+}
+package mypack;
+        import pack.*;
+class B{
+    public static void main(String args[]){
+        A obj = new A();//Compile Time Error
+        obj.msg();//Compile Time Error
+    }
+}*/
+
+/*
+package pack;
+public class A{
+    protected void msg(){System.out.println("Hello");}
+}
+
+package mypack;
+        import pack.*;
+class B extends A{
+    public static void main(String args[]){
+        B obj = new B();
+        obj.msg();
+    }
+}
+*/
+
+/*package pack;
+public class A{
+    public void msg(){System.out.println("Hello");}
+}
+package mypack;
+        import pack.*;
+class B{
+    public static void main(String args[]){
+        A obj = new A();
+        obj.msg();
+    }
+}  */
+
