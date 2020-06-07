cbfa9977932ed19971e4e0f1b9af7aec6a49aaab
JOY <joy@JOYs-iMac-Pro.local>
2020-06-07 14:37:54 +0700

    add code

---
+++ Module2_Java/AccessModifierAndStatic/src/ThucHanh/StaticMethod.java
@@ -0,0 +1,39 @@
+package ThucHanh;
+package com.codegym;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+    //constructor to initialize the variable
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    //static method to change the value of static variable
+    static void change() {
+        college = "CODEGYM";
+    }
+
+    //method to display values
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
+
+package com.codegym;
+
+public class StaticMethod {
+    public static void main(String args[]) {
+        Student.change();//calling change method
+//creating objects
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+//calling display method
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
