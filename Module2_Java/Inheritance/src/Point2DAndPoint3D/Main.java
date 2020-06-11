f30504b9f205b3426554e39ccec88d3930cfa858
Viet Nguyen Thai <viet@viets-MacBook-Pro-16.local>
2020-06-11 20:11:59 +0700

    add code

---
+++ Module2_Java/Inheritance/src/Point2DAndPoint3D/Main.java
@@ -0,0 +1,10 @@
+package Point2DAndPoint3D;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(1.3f, 3.0f);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D(1.2f, 1.3f, 1.4f);
+        System.out.println(point3D.toString());
+    }
+}
---
+++ Module2_Java/Inheritance/src/Point2DAndPoint3D/Point3D.java
@@ -0,0 +1,37 @@
+package Point2DAndPoint3D;
+
+public class Point3D extends Point2D {
+    float z;
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setX(x);
+        super.setY(y);
+        this.setZ(z);
+    }
+
+    public float[] getXYZ() {
+        return new float[] {super.getX(),super.getY(),z};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + super.getX() + ", " + super.getY() + ", " + z + ")";
+    }
+}
